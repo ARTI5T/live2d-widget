0b975922d6b0e1e1c4f92ea8bf2d105d3c84b6eb
ARTI5T <Huangyinwei@live.cn>
2021-08-24 01:18:09 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -33,8 +33,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			apiPath: "https://alpha.sjmc.club:8001/live2d/",
+			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
 }
